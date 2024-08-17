92985ebb547d53013c6329ea7b632998db5d4c3c
seonyo <jsy6535@naver.com>
2024-08-18 02:06:36 +0900

    add onclick function

--- components/movie.tsx
+++ components/movie.tsx
@@ -1,5 +1,8 @@
+"use client"
+
 import Link from "next/link";
 import styles from "../styles/movie.module.css"
+import { useRouter } from "next/navigation";
 
 interface IMovieProps {
     title:string;
@@ -8,10 +11,14 @@ interface IMovieProps {
 }
 
 export default function Movie({title, id, poster_path}: IMovieProps) {
+    const router = useRouter();
+    const onClick = () => {
+        router.push(`/movies/${id}`);
+    }
     return(
         <div className={styles.movie}>
             <div key={id}>
-                <img src={poster_path} alt={title} />
+                <img src={poster_path} alt={title} onClick={onClick}/>
                 <Link href={`/movies/${id}`}>{title}</Link>
             </div>
     </div>
